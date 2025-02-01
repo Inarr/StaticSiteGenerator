479784790850f492e105489c5ae400799117b806
Inarr Dobrey <inarrd@gmail.com>
2025-02-01 08:30:15 -0500

    HTML Test completed

--- public/src/test_htmlnode.py
+++ public/src/test_htmlnode.py
@@ -3,12 +3,19 @@ from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        
+        node1 = HTMLNode('p', 'This is a text', ['pic','para'], {'href': 'https://www.google.com', 'target':'_blank'})
+        node2 = HTMLNode('p', 'This is a text', ['pic','para'], {'href': 'https://www.google.com', 'target':'_blank'})
+        #node3 = HTMLNode('p', 'This is a text', ['pic','para'])
+        self.assertEqual(node1, node2)
 
     def test_notEq(self):
-        
+        node1 = HTMLNode('p', 'This is a text', ['pic','para'], {'href': 'https://www.google.com', 'target':'_blank'})
+        node2 = HTMLNode('p', 'This is a text', ['pic','para'])
+        self.assertNotEqual(node1, node2)
 
     def test_props_to_html(self):
+        node1 = HTMLNode('', '', [], {"href": "https://www.google.com","target": "_blank",})
+        node1.props_to_html()
         
 
 
