1530aa47343c39be273fd2b5d66ea9232d9c183e
Inarr <inarrd@gmail.com>
2025-02-19 14:30:48 -0500

    Update SplitNodeDelimiter.py

--- public/src/SplitNodeDelimiter.py
+++ public/src/SplitNodeDelimiter.py
@@ -1,1 +1,17 @@
+from textnode import TextType, TextNode
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+  new_list=[]
+  match delimiter:
+    case '**':
+      block = TextType.BOLD
+    case '*':
+      block = TextType.ITALIC
+    case '`':
+      block = TextType.CODE
+  for node in old_nodes:
+    if node.text_type != TextType.NORMAL:
+      new_list.append(node)
+    if node.text.count(delimiter) % 2 != 0:
+      raise Exception('Invalid Markdown Syntax')
+    
