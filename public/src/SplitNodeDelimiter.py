b7be5e19d4d1d2ba80ac31872361bc143560039b
Inarr <inarrd@gmail.com>
2025-02-19 15:02:28 -0500

    Finished split_nodes_delimiter

--- public/src/SplitNodeDelimiter.py
+++ public/src/SplitNodeDelimiter.py
@@ -1,17 +1,31 @@
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-  new_list=[]
-  match delimiter:
-    case '**':
-      block = TextType.BOLD
-    case '*':
-      block = TextType.ITALIC
-    case '`':
-      block = TextType.CODE
+  master_list=[] 
   for node in old_nodes:
-    if node.text_type != TextType.NORMAL:
-      new_list.append(node)
+    new_list=[]
     if node.text.count(delimiter) % 2 != 0:
       raise Exception('Invalid Markdown Syntax')
+    if node.text_type != TextType.NORMAL:
+      new_list.append(node)
+    else:
+      # Splitting Text according to the Delimiter
+      temp_list = node.text.split(delimiter)
+      # If the first string was blocked, the TextTye is every odd position in temp_list
+      if node.text.find(delimiter) == 0:
+        for i in range(len(temp_list)):
+          if i%2 == 0:
+            new_list.append(TextNode(temp_list[i], text_type))
+          else:
+            new_list.append(TextNode(temp_list[i], TextType.NORMAL))
+      else: # Delimiter in every even position
+         for i in range(len(temp_list)):
+          if i%2 != 0:
+            new_list.append(TextNode(temp_list[i], text_type))
+          else:
+            new_list.append(TextNode(temp_list[i], TextType.NORMAL)
+        master_list.append(new_list)
+      return master_list
+      
+        
     
