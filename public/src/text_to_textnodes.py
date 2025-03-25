44bd572352919705ab37c067fd6f8dd2af8aab70
Inarr <inarrd@gmail.com>
2025-03-25 10:25:36 -0400

    Update text_to_textnodes.py

--- public/src/text_to_textnodes.py
+++ public/src/text_to_textnodes.py
@@ -1,1 +1,11 @@
-def 
+from textnode import TextType, TextNode
+from SplitNodeDelimiter import *
+from SplitNodeImages import *
+from SplitNodeLinks import *
+
+def text_to_textnodes(text):
+  tempList1 = split_nodes_delimiter([text],'**', TextType.BOLD)
+  tempList2 = split_nodes_delimiter([tempList1],'_', TextType.ITALIC)
+  tempList3 = split_nodes_delimiter([tempList2],'`', TextType.CODE)
+  templist4 = split_nodes_image(tempList3)
+  return split_nodes_link(templist4)
