bcd3b84c8d04e738607288a76727e25062b62c83
Inarr <inarrd@gmail.com>
2025-04-03 08:07:59 -0400

    Update test_markdown_to_blocks.py

--- public/src/test_markdown_to_blocks.py
+++ public/src/test_markdown_to_blocks.py
@@ -2,11 +2,13 @@ import unittest
 from markdown_to_blocks import *
 
 def test_markdown_to_blocks(self):
+    # md has double space
     md = """
 This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
+
 
 - This is a list
 - with items
