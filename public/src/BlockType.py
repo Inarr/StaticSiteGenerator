c887458fb8798faa04e9ab670b978c83c56677e4
Inarr <inarrd@gmail.com>
2025-04-08 10:09:02 -0400

    Update BlockType.py

--- public/src/BlockType.py
+++ public/src/BlockType.py
@@ -1,9 +1,22 @@
 from enum import Enum
 
 class BlockType(Enum):
-  paragraph = 'paragraph'
-  heading = 'heading'
-  code = 'code'
-  quote = 'quote'
-  unordered_list = 'unordered_list'
-  ordered_list = 'ordered_list'
+  PARAGRAPH = 'paragraph'
+  HEADING = 'heading'
+  CODE = 'code'
+  QUOTE = 'quote'
+  UNORDERED_LIST = 'unordered_list'
+  ORDERED_LIST = 'ordered_list'
+
+def block_to_block_type(block):
+  if block[0] =='*':
+    for i in range(1,7):
+      if block[i] == ' ':
+        return BlockType.HEADING
+      elif i == 6:
+        return BlockType.PARAGRAPH
+    if block[:2] == block[-2:] and block[:2] == '```':
+      return BlockType.CODE
+        
+      
+  
