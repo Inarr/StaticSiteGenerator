104c700fd350ff609a4494567a994eea51309cd9
Inarr <inarrd@gmail.com>
2025-04-23 07:24:44 -0400

    Update MarkdownToHTMLNode.py

--- public/src/MarkdownToHTMLNode.py
+++ public/src/MarkdownToHTMLNode.py
@@ -1,4 +1,10 @@
 from markdown_to_blocks import markdown_to_blocks
+from BlockType import *
 
 def markdown_to_html_node(markdown):
   blocks = markdown_to_blocks(mrkdown)
+  block_type_dict = {}
+  for block in blocks:
+    block_type_dict[block] = block_to_block_type(block)
+    
+    
